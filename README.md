1304c93428242a0bc9bb32ed94b1ccac4fea56cb
alexdin <mralexdin@gmail.com>
2017-08-21 23:29:41 +0300

    add .gitignore file

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # yii2-file-session
+This is an expanding class of web sessions. This class supports retrieving data on session id and deleting it.
+
+Installation
+------------
+```
+php composer.phar require --prefer-dist alexdin/yii2-file-session
+```
+Configure 
+-----
+Configure file session
+add this string to components in config main.php file (main-local.php).
+```php
+ 'session' => [
+             // this is the name of the session cookie used for login on the backend
+             'class'=>'alexdin\file-session',
+             'name' => 'session-name',
+             // dir for your path session
+             // create migrate if dir is not exists or use "mkdir" function
+             'savePath' => __DIR__ . '/../runtime/sessions',
+         ],
+ ```
+ Usage
+ -----
+
+```php
+Yii::$app->session->destroySession($id)
+```
+Clears and deletes the session by id
+```php
+Yii::$app->session->readSession($id)
+```
+Reads the session from the file and returns the serialized data in the string
+```php
+Yii::$app->session->getSessionData($id)
+```
+Returns an array of session data by  id
+```php
+Yii::$app->session->getSessionFileName($id)
+```
+Returns the session file name by id
---
+++ Session.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexdin
+ * Date: 21.08.17
+ * Time: 22:43
+ */
+
+
+class Session extends \yii\web\Session
+{
+
+    /**
+     * @param string $id session id key
+     * @return bool
+     */
+    public function destroySession($id)
+    {
+        //save current session
+        $sessionId = session_id();
+        $this->close();
+        $this->setId($id);
+        $this->open();
+        session_unset();
+        session_destroy();
+        $this->setId($sessionId);
+        return true;
+    }
+
+    /** returned Array with session data. It is not standart method,
+     * if you use other class , change mathod name in your code.
+     * @param string $id session id key
+     * @return array|true
+     */
+
+    public function getSessionData ($id)
+    {
+
+        $my_session = $_SESSION;
+        $data = $this->readSession($id);
+        session_decode($data);
+        $result = $_SESSION;
+        $_SESSION = $my_session;
+        return $result;
+    }
+
+    /**
+     * reading session file fom dir
+     * @param string $id
+     * @return string|null
+     */
+
+    public function readSession($id)
+    {
+        $file = session_save_path().'/'.$this->getSessionFileName($id);
+        if(file_exists($file))
+        {
+            return @file_get_contents($file);
+        }else{
+            return null;
+        }
+
+    }
+
+    /** returned session file name by session id (add prefix "sess_")
+     * It looks like the default session files look like this ==> sess_yoursessionid
+     * @param $id
+     * @return string
+     */
+
+    public function getSessionFileName($id)
+    {
+        return 'sess_'.$id;
+    }
+
+}
